f32bae8f93bf8b49a85d0b103ec6ab69aeeedda9
Scott Braaten <scottnicholasbraaten@gmail.com>
2023-04-28 21:23:57 -0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,23 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const stored = localStorage.getItem('isLoggedIn');
+
+    if (stored && stored === '1') setIsLoggedIn(true);
+  }, []);
+
+  // when array is empty in useEffect, effect is only ran once,
+  // when the app first starts.
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // We should of course check email and pas
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
